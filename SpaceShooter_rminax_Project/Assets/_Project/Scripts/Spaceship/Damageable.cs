8cb95ae434cde1b18b257228f121b39073e02ce3
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2023-06-18 17:16:16 +0300

    minor update

---
+++ SpaceShooter_rminax_Project/Assets/_Project/Scripts/Spaceship/Damageable.cs
@@ -0,0 +1,15 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace _Project.Scripts.Spaceship
+{
+    public class Damageable : NetworkBehaviour
+    {
+        [SerializeField] private Health _health;
+
+        public void DealDamage(float dealToDamage)
+        {
+            _health.Remove(dealToDamage);
+        }
+    }
+}
---
+++ SpaceShooter_rminax_Project/Assets/_Project/Scripts/Spaceship/Health.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+namespace _Project.Scripts.Spaceship
+{
+    public class Health : NetworkBehaviour
+    {
+        public event EventHandler<DeathEventArgs> OnDeath; 
+        public event EventHandler<HealthChangedEventArgs> OnHealthChanged; 
+        
+        [SerializeField] private float _maxHealth = 100;
+
+        public bool IsDead => _currentHealth <= 0f;
+
+        #region Sync Variables
+
+        [SyncVar(hook = nameof(OnHealthUpdated))]
+        private float _currentHealth;
+        
+        private void OnHealthUpdated(float _, float newHealth)
+        {
+            OnHealthChanged?.Invoke(this, new HealthChangedEventArgs
+            {
+                Health = _currentHealth,
+                MaxHealth = _maxHealth
+            });
+        }
+
+        #endregion
+
+        #region Server Callbacks
+
+        public override void OnStartServer()
+        {
+            _currentHealth = _maxHealth;
+        }
+
+        #endregion
+
+        [Server]
+        public void Add(float value)
+        {
+            value = Mathf.Max(value, 0);
+
+            _currentHealth = Mathf.Min(_currentHealth + value, _maxHealth);
+        }
+        
+        [Server]
+        public void Remove(float value)
+        {
+            value = Mathf.Max(value, 0);
+
+            _currentHealth = Mathf.Max(_currentHealth - 0, 0);
+
+            if (_currentHealth <= 0)
+            {
+                OnDeath?.Invoke(this, new DeathEventArgs{ConnectionToClient = connectionToClient});
+
+                RPC_HandleDeath();
+            }
+        }
+
+        private void RPC_HandleDeath()
+        {
+            gameObject.SetActive(false);
+        }
+
+        #region Base methods
+
+        [ServerCallback]
+        private void OnDestroy()
+        {
+            OnDeath?.Invoke(this, new DeathEventArgs{ConnectionToClient = connectionToClient});
+        }
+
+        #endregion
+    }
+
+    public class DeathEventArgs
+    {
+        public NetworkConnection ConnectionToClient;
+    }
+
+    public class HealthChangedEventArgs
+    {
+        public float Health;
+        public float MaxHealth;
+    }
+}
